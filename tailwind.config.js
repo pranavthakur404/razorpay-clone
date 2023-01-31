951674892d9215b3f057fc7dbf47ec943833b87f
pranavthakur404 <pranavufc@gmail.com>
2023-01-31 14:32:01 +0530

    Done with nav section

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {fontFamily:{
+      mullish:["mulish","san-serif"],
+    }},
+    colors:{
+      deepBlue:"#02042a",
+      lightBlue:"#2b84ea",
+      lightBlue300:"#4b94ed",
+      lightBlue500:"#0b72e7",
+      greenLight:"#61cea6",
+      greyText:"#818597",
+      lightGray:"#e2e2e2",
+      greyBlueHead:"#162f56",
+      grey2:"#525a76",
+      white: "#FFF",
+    }
+  },
+  plugins: [],
+}
